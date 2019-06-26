e2d7df87e66ac566e028a79bbc3cdeb0696227f4
Lars Tore Carlsen <lars-tore.carlsen@knowit.no>
2019-06-26 19:39:27 +0200

    La til Webserver.

--- build.sbt
+++ build.sbt
@@ -3,3 +3,9 @@ name := "Hemsedalen"
 version := "0.1"
 
 scalaVersion := "2.13.0"
+
+libraryDependencies ++=  Seq(
+  "com.typesafe.akka" %% "akka-http"   % "10.1.8",
+  "com.typesafe.akka" %% "akka-stream" % "2.5.23" // or whatever the latest version is
+)
+
---
+++ src/main/scala/WebServer.scala
@@ -0,0 +1,30 @@
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+
+import scala.io.StdIn
+
+object WebServer {
+
+  implicit val system = ActorSystem("product-management-service")
+  implicit val materializer = ActorMaterializer()
+
+
+  implicit val executionContext = system.dispatcher
+  val route =
+    path("hemsedalen") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      }
+    }
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
+}
